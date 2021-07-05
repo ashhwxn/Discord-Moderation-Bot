dac47ec8ce1b7fe18304a8e6640ce62e13b5a681
ashhwxn <86062169+ashhwxn@users.noreply.github.com>
2021-07-05 18:47:57 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-exports.TOKEN = "your bot token";
+exports.TOKEN = "ODYxNTczODg5MDQ1MTY4MTU4.YOLxNg.WucZT9II4f_kvHdnHnOdHHD2KW0";
 
-exports.PREFIX = 'YOUR PREFIX';
+exports.PREFIX = '$$';
 
 exports.OWNER_ID = "YOUR ACC ID"
